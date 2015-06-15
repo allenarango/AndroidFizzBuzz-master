c36b26d4580acc79589efd4e220c9a6072b0d403
allenarango <ararango@Allens-MacBook-Air.local>
2015-06-14 19:54:30 -0500

    FizzBuzz Initial Commit

---
+++ README.md
@@ -0,0 +1,24 @@
+AndroidFizzBuzz
+===============
+
+This is the source code for the Thinkful Android Fizz Buzz App for Unit 1. 
+
+This app is purposely incomplete. With your newly learned java skills your task is to complete this project and run it in the emulator.
+
+Here are the requirements:
+
+1. Declare `String` variables for the words "Fizz", "Buzz", and "FizzBuzz". (Unit 1 Lesson 2 Assignment 1)
+2. Create a `for` loop inside the doButton method that will iterate the number of times given by the user.(Unit 1 Lesson 2 Assignment 2)
+![](FizzBuzzForLoop.png)
+
+3. For each iteration of the loop, display the string of either the number, "Fizz", "Buzz", or "FizzBuzz" depending on the value. (Unit 1 Lesson 2 Assignment 3)
+![](FizzBuzzCorrect.png)
+
+4. Create an array of indicies for each "Fizz", and "Buzz" word, then display the arrays in the console. (Unit 1 Lesson 2 Assignment 4)
+![](BuzzArrayPrint.png)
+
+Here's the definition from wikipedia on what is [Fizz Buzz](http://en.wikipedia.org/wiki/Fizz_buzz)
+
+
+> Fizz buzz is a group word game for children to teach them about division.Players take turns to count incrementally, replacing any number divisible by three with the word "fizz", and any number divisible by five with the word "buzz"... Numbers divisible by both become "fizz buzz".
+
---
+++ app/src/main/java/com/thinkful/fizzbuzzunit1/MainActivity.java
@@ -0,0 +1,106 @@
+package com.thinkful.fizzbuzzunit1;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.view.View;
+
+
+public class MainActivity extends Activity {
+
+    TextView textView;
+    String fizzString = "Fizz";
+    String buzzString = "Buzz";
+    String fizzBuzzString = "FizzBuzz";
+    String valueString;
+    EditText editText;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    int [] fizzArray = new int [10];
+    int fizzIndex = 0;
+    int [] buzzArray = new int [10];
+    int buzzIndex = 0;
+
+    public void doButton(View view) {
+        // Implement the function of the button here
+        // Use numberOfValues, and create a string array of both the numbers and words,
+        // replacing the appropriate numbers with the words fizz or buzz
+
+        editText = (EditText) findViewById(R.id.editText); // user input
+        textView = (TextView) findViewById(R.id.fizzText); // where the text will display
+        textView.setText("" + "\n");
+
+        String valueString;
+
+        int numberOfValues = Integer.parseInt(editText.getText().toString());
+
+        for (int i = 1; i <= numberOfValues; i++) {
+            checkValue(i);
+        }
+        for (int i = 0; i < fizzArray.length; i++) {
+            Log.i("MyData", fizzArray[" + i + "] = " + fizzArray[i]);
+        }
+    }
+
+
+    public void checkValue(int i){
+
+        String value;
+        if (i % 5 == 0 && i % 3 == 0) { // divisble by 5 & 3
+            valueString = fizzBuzzString;
+        }
+        else if (i % 3 == 0) { // divisible by 3
+            valueString = fizzString;
+            fizzArray[fizzIndex] = i;
+            fizzIndex++;
+        }
+        else if (i % 3 == 0) { // divisible by 3
+            valueString = fizzString;
+        }
+        else if (i % 5 == 0) {
+            valueString = buzzString;
+            buzzArray[buzzIndex] = i;
+            buzzIndex++;
+        }
+        else if (i % 5 == 0) { // divisible by 5
+            valueString = buzzString;
+        }
+        else {
+            valueString = String.valueOf(i);
+        }
+        textView.append(valueString + "\n"); // display in the textView
+        } // end for loop
+    }
